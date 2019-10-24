fd59c43f16fd60be3bdaab41db295f9734f3394c
AndreaVidal <andreavidals@gmail.com>
2019-10-24 14:57:07 -0500

    Add files via upload

---
+++ Dictionary/get_phonemes_cmu.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+##################################
+#	READ WORDS OVV TXT FILE	     #
+##################################
+path_file = 'oovs_found.txt';
+file_txt = open(path_file,'r');
+file_txt_list = file_txt.readlines();
+file_txt.close()
+
+######################################
+#			NEW DICTIONARY 			 #
+######################################
+#Create file for saving the words and their pronunciation
+new_words_dictionary = open('wordsPhonemes.txt','w');
+
+#Create file for saving words that are still out of vocabulary
+wovv = open('D:\\Lip_generation\\MSP_Face_alignment\\Align\\wovv_words3.txt','w');
+
+#Procesing the txt file
+for word in file_txt_list:
+
+	query_word = word.strip('\n').lower();
+	url = 'http://www.speech.cs.cmu.edu/cgi-bin/cmudict?in='+ query_word +'&stress=-s' #Take into account the stress of the word
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, "html.parser")
+
+	rows = soup.find_all("tt")
+
+	#If the row start with "http" means that word to phoneme was not found in the dictionary
+	if not rows[1].get_text().startswith('http'):
+		phonemes = rows[1].get_text()[:-2];
+		new_words_dictionary.write(word.strip('\n').upper() + '\t' +phonemes + '\n');
+	else:
+		wovv.write(word);
+
+new_words_dictionary.close()
+wovv.close()
+
